97d4e59fe2a933b05199b05f94402d3f20b8aa00
Pyotr Ryzhov <maestro_pete@inbox.ru>
2022-04-13 12:29:27 +0300

    styled mobile menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,30 @@
 function ready(fn) {
-  if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
+  if (
+    document.attachEvent
+      ? document.readyState === 'complete'
+      : document.readyState !== 'loading'
+  ) {
     fn();
   } else {
     document.addEventListener('DOMContentLoaded', fn);
   }
 }
 
-ready(function(){
-  console.log('DOM ready');
+ready(function () {
+  const MenuBurgerBtn = document.querySelector('#menu-burger');
+  const MainMenu = document.querySelector('#main-nav');
+  if (MenuBurgerBtn) {
+    MenuBurgerBtn.addEventListener('click', ev => {
+      ev.target.classList.toggle('active');
+      MainMenu.classList.toggle('active');
+    });
+  }
+
+  const MenuItemTriggers = document.querySelectorAll('.main-nav__item--trigger');
+  MenuItemTriggers.forEach(item => {
+    item.addEventListener('click', ev => {
+      item.classList.toggle('active');
+      item.nextElementSibling.classList.toggle('active')
+    })
+  });
 });
